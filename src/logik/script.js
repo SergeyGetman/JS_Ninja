1b2762e950dd0ec4614048834f0521eb14352f68
AndriiIladii <iladiy21@gmail.com>
2025-06-13 21:45:13 +0300

    commit from last lesson

--- src/logik/script.js
+++ src/logik/script.js
@@ -1,8 +1,33 @@
-function getData(ev) {
-	ev.preventDefault();
-	console.log('EV', ev);
+// function getData(ev) {
+//   ev.preventDefault();
+//   console.log("EV", ev);
+// }
+
+// function goAheadLessons() {
+//   return (window.location.href = "./lessons.html");
+// }
+
+function validateHello(greetings) {
+  let res = "";
+  const language = {
+    hello: "english",
+    ciao: "italian",
+    salut: "frenc",
+    hallo: "german",
+    hola: "spanish",
+    ahoj: "czech republic",
+    czesc: "polish",
+  };
+
+  const reskey = {
+    language: "",
+  };
+
+  if (language.hasOwnProperty(greetings.toLowerCase())) {
+    reskey.language = language[greetings];
+  }
+
+  return reskey.language;
 }
 
-function goAheadLessons() {
-	return (window.location.href = './lessons.html');
-}
+validateHello("hallo");
