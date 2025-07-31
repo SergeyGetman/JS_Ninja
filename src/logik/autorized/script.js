5b3b5c3627f4e63a82d6f00eebc6931853a6d939
anonimusZero <anonimus_zero@proton.me>
2025-08-01 00:51:09 +0300

    before validetion

--- src/logik/autorized/script.js
+++ src/logik/autorized/script.js
@@ -2,6 +2,23 @@ function signIn() {
 	alert('the implementation is not ready yet');
 }
 
+const updateFormData = {
+	nickName: '',
+	password: ''
+};
+
+function getFormData() {
+	const form = document.querySelector('form');
+	if (form) {
+		updateFormData.nickName = form.elements.emailID.value;
+		updateFormData.password = form.elements.passwordID.value;
+
+		console.log('this is data', updateFormData);
+		form.reset();
+		hideForm();
+	}
+}
+
 const hideForm = () => {
 	const getForID = document.querySelector('.form-content');
 	const spinnerBlock = document.getElementById('spinnerId');
